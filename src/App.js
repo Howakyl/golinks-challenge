d6d6f7347c25684644b8034bcb3d7e16af2fbc73
Howakyl <howakyl@gmail.com>
2021-05-15 14:05:59 -0700

    testing reaching github api, environment vars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
 
 function App() {
+
+  async function getData () {
+    const res = await fetch('https://api.github.com/orgs/Netflix/repos', {
+      headers: {
+        authorization: process.env.REACT_APP_API_KEY,
+      },
+    });
+    const data = await res.json();
+    console.log(data)
+  }
+  // getData()
+
   return (
     <div>hello world</div>
   );
