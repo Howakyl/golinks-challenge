83d267404424fcd33063cf0485bbbecfd64a04da
Howakyl <howakyl@gmail.com>
2021-05-15 19:04:04 -0700

    RepoItem button, on click transitions Modal component in

--- src/components/Repos/RepoItem.js
+++ src/components/Repos/RepoItem.js
@@ -1,7 +1,16 @@
 import "./RepoItem.css";
 import RepoStats from "./RepoStats";
+import Modal from "../UI/Modal";
+import { useState } from "react";
 
 const RepoItem = (props) => {
+  const [modalToggle, setModalToggle] = useState(false);
+
+  const modalHandler = (e) => {
+    e.preventDefault();
+    setModalToggle(true);
+  }
+
   return (
     <li className="repo-list__item">
       <div className="repo-list__info">
@@ -11,7 +20,8 @@ const RepoItem = (props) => {
         </div>
         <RepoStats repo={props.repo} />
       </div>
-      <button>+</button>
+      <button onClick={modalHandler} type="button">+</button>
+      <Modal show={modalToggle}>here is some content</Modal>
     </li>
   );
 };
