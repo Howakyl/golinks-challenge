e7da453af0507c5963dae8726c9deb8ef30a0593
Howakyl <howakyl@gmail.com>
2021-05-15 19:22:47 -0700

    modal fades in on button click

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,8 +5,7 @@ const Modal = (props) => {
     <div
       className="modal"
       style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? 1 : 0,
+        visibility: props.show ? 'visible' : 'hidden'
       }}
     >
       {props.children}
