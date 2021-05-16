9b1e79e185aae0d2a6ed5f92178b8e291ee58026
Howakyl <howakyl@gmail.com>
2021-05-15 17:08:29 -0700

    separated RepoList info and stats, spaced between both sections for a more consistent look independent of the text content

--- src/components/RepoItem.js
+++ src/components/RepoItem.js
@@ -4,12 +4,20 @@ const RepoItem = (props) => {
   return (
     <li className="repo-list__item">
       <div className="repo-list__info">
-        <h3>{props.repo.name}</h3>
-        <p>{props.repo.description}</p>
-        <div className="repo-list_info__stars">
-          <i class="fas fa-star"></i>
-          <span>{props.repo.stargazers_count}</span>
+        <div>
+          <h3>{props.repo.name}</h3>
+          <p>{props.repo.description}</p>
         </div>
+        <section className="repo-list_stats">
+          <div className="repo-list__info__stars">
+            <i className="fas fa-star"></i>
+            <span>{props.repo.stargazers_count}</span>
+          </div>
+          <div className="repo-list__info__forks">
+            <i className="fas fa-code-branch"></i>
+            <span>{props.repo.forks_count}</span>
+          </div>
+        </section>
       </div>
       <button>+</button>
     </li>
