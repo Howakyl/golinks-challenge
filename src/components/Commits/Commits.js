7db9eb8b93c7766e3019facf5eeb4e0a85c350c0
Howakyl <howakyle96@live.com>
2021-05-16 16:55:07 -0700

    pulling commit data from repos, rendering either commmit usernames or names

--- src/components/Commits/Commits.js
+++ src/components/Commits/Commits.js
@@ -1,10 +1,46 @@
 import { useState, useEffect } from "react";
-const Commits = () => {
-  // useEffect(() => {
+import classes from "./Commits.module.css";
 
-  // },[])
+const Commits = (props) => {
+  const [commits, setCommits] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  return <div>test</div>;
+  const trimmedCommitUrl = props.repo.commits_url.replace("{/sha}", "").trim();
+
+  const getCommits = async () => {
+    const res = await fetch(trimmedCommitUrl, {
+      headers: {
+        authorization: process.env.REACT_APP_API_KEY,
+      },
+    });
+    const commitData = await res.json();
+    setCommits(commitData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCommits();
+  }, []);
+
+  return (
+    <div>
+      {loading && <h2>Loading...</h2>}
+
+      {!loading && (
+        <div className={classes.commits}>
+          {commits.map((commit) => (
+            <div key={commit.sha}>
+              {commit.author != null ? (
+                <p>{commit.author.login}</p>
+              ) : (
+                <p>{commit.commit.author.name}</p>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Commits;
