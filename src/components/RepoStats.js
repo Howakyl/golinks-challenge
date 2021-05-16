27d4908dec8e118c6bafde1b9f1a1fdc98b98c21
Howakyl <howakyl@gmail.com>
2021-05-15 18:18:06 -0700

    RepoStats now has date repo was created, formatted in readable format

--- src/components/RepoStats.js
+++ src/components/RepoStats.js
@@ -1,8 +1,15 @@
-import './RepoStats.css';
+import "./RepoStats.css";
 
 const RepoStats = (props) => {
+  const date = new Date(props.repo.created_at);
+  const day = date.toLocaleString("en-US", { month: "short", day: "2-digit" })
+  const year = date.getFullYear();
+
   return (
     <section className="repo-stats">
+      <div>
+        <span>{props.repo.language}</span>
+      </div>
       <div className="repo-stats__stars">
         <i className="fas fa-star"></i>
         <span>{props.repo.stargazers_count}</span>
@@ -10,6 +17,9 @@ const RepoStats = (props) => {
       <div className="repo-stats__forks">
         <i className="fas fa-code-branch"></i>
         <span>{props.repo.forks_count}</span>
+      </div>
+      <div>
+        <span>Created At: {day}, {year}</span>
       </div>
     </section>
   );
