c84b0f4d7d54d1252d345676a0d9cb9040ea720d
Howakyl <howakyl@gmail.com>
2021-05-17 13:32:18 -0700

    rendering user avatars, github logo as fallback avatar

--- src/components/Commits/Commit.js
+++ src/components/Commits/Commit.js
@@ -2,12 +2,30 @@ import classes from "./Commit.module.css";
 
 const Commit = (props) => {
   return (
-    <div>
-      {props.commit.author != null ? (
-        <p>{props.commit.author.login}</p>
-      ) : (
-        <p>{props.commit.commit.author.name}</p>
-      )}
+    <div className={classes.container}>
+      <figure>
+        {props.commit.author ? (
+          <img
+            src={props.commit.author.avatar_url}
+            alt=""
+            className={classes.avatar}
+          />
+        ) : (
+          <img
+            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+            alt="github logo"
+            className={classes.avatar}
+          />
+        )}
+      </figure>
+
+      <div>
+        {props.commit.author != null ? (
+          <p>{props.commit.author.login}</p>
+        ) : (
+          <p>{props.commit.commit.author.name}</p>
+        )}
+      </div>
     </div>
   );
 };
