e47c49fc2a3d685998faed176137f9cd78203aeb
Howakyl <howakyl@gmail.com>
2021-05-17 15:09:27 -0700

    tooltip for hash

--- src/components/Commits/CommitLinks.js
+++ src/components/Commits/CommitLinks.js
@@ -5,7 +5,7 @@ const CommitLinks = (props) => {
 
   return (
     <div className={classes.container}>
-      <a href={props.commit.html_url} target="_blank" rel="noreferrer">
+      <a href={props.commit.html_url} target="_blank" rel="noreferrer" title={props.commit.sha}>
         <button className={classes.hash}>{trimmedHash}</button>
       </a>
     </div>
