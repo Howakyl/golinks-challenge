f3a19616f206d8720ad66950bb10e105dec701da
Howakyl <howakyl@gmail.com>
2021-05-17 17:34:01 -0700

    converted RepoStats to module

--- src/components/Repos/RepoStats.js
+++ src/components/Repos/RepoStats.js
@@ -1,4 +1,4 @@
-import "./RepoStats.css";
+import classes from "./RepoStats.module.css";
 
 const RepoStats = (props) => {
   const date = new Date(props.repo.created_at);
@@ -6,15 +6,15 @@ const RepoStats = (props) => {
   const year = date.getFullYear();
 
   return (
-    <section className="repo-stats">
+    <section className={classes.statsContainer}>
       <div>
         <span>{props.repo.language}</span>
       </div>
-      <div className="repo-stats__stars">
+      <div className={classes.stars}>
         <i className="fas fa-star"></i>
         <span>{props.repo.stargazers_count}</span>
       </div>
-      <div className="repo-stats__forks">
+      <div className={classes.forks}>
         <i className="fas fa-code-branch"></i>
         <span>{props.repo.forks_count}</span>
       </div>
