76cba6fd52222a3cede2faa4f57eb8aba75e4fa5
Howakyl <howakyl@gmail.com>
2021-05-17 19:28:47 -0700

    search input successfully retrieving json data from org url

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,19 +3,40 @@ import classes from "./Search.module.css";
 
 const Search = () => {
   const [searchInput, setSearchInput] = useState("");
-
-  const handleClick = () => {
-    console.log(searchInput);
-  };
+  const [org, setOrg] = useState({});
 
   const handleChange = (e) => {
     setSearchInput(e.target.value);
   };
 
+  const handleClick = async () => {
+    console.log(searchInput);
+
+    try {
+      const result = await fetch(`https://api.github.com/orgs/${searchInput}`, {
+        headers: {
+          authorization: process.env.REACT_APP_API_KEY
+        }
+      });
+      const data = await result.json();
+      setOrg(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log('ORG: ', org)
   return (
     <div className={classes.searchContainer}>
-      <input type="text" placeholder="search orgs:" onChange={handleChange}  className={classes.searchInput}/>
-      <button onClick={handleClick} className={classes.searchButton}>Search</button>
+      <input
+        type="text"
+        placeholder="search orgs:"
+        onChange={handleChange}
+        className={classes.searchInput}
+      />
+      <button onClick={handleClick} className={classes.searchButton}>
+        Search
+      </button>
     </div>
   );
 };
